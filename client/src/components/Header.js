7a87e8aa8bc894ec0e2f0e3381ab257f83be4738
Matt Draghi <mtd5264@gmail.com>
2022-04-05 17:54:45 -0400

    Created menu icon

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,19 @@
-function Header(){
-    return(
-        <div className="header-container">
+import "../styles/Header.css"
 
-        </div>
+function Header(){
+
+    const showSidebar = () =>{
+        
+    }
+
+    return(
+        <header className="header-container">
+            <div className="menu-button-container" onClick={showSidebar}>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+            </div>
+        </header>
     )
 }
 
