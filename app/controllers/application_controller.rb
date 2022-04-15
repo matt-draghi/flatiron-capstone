d073c09835c69038cb46dea54e0c0df5b3275615
Matt Draghi <mtd5264@gmail.com>
2022-04-15 10:26:58 -0400

    Fix for requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
     include ActionController::Cookies
     # include ::ActionController::Serialization
   end
