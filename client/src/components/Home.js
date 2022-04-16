ff75beb5fb2c478e40f8ed290e402d79ea7e4c4a
Matt Draghi <mtd5264@gmail.com>
2022-04-16 18:21:52 -0400

    Exercise of the day implemented in frontend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,27 +1,46 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "../styles/Home.css"
 
 function Home(){
+    const [eotd, setEotd] = useState(localStorage.getItem('eotd'))
 
     useEffect(()=>{
         fetch('/api/exercises/daily')
         .then(response => response.json())
-        .then(exercise => console.log(exercise))
+        .then(exercise => {
+            setEotd(exercise)
+            localStorage.setItem('eotd', exercise)
+        })//console.log(exercise))
     },[])
+
+    const homeDisplay = () => {
+        if(eotd){
+            return(
+                <div className="eotd-container">
+
+                    <h1>Exercise of the Day</h1>
+                    <div className="eotd">
+                        {/* placeholder for workout of the day video and information*/}
+                        <div className="eotd-video">
+                            <iframe width="100%" height="100%" src={eotd?.demo_url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
+                        <div className="eotd-description">
+                            <h3>{eotd.name}</h3>
+                            <p>{eotd.description}</p>
+                        </div>
+                    </div>
+                </div>
+
+            )
+        }
+        else{
+           return  <h1>Loading...</h1>
+        }
+    }
 
     return(
         <div className="home-container">
-            <div className="eotd-container">
-                <h1>Exercise of the Day</h1>
-                <div className="eotd">
-                    {/* placeholder for workout of the day video and information*/}
-                    <div className="eotd-video"></div>
-                    <div className="eotd-description">
-                        <h3>Workout Name</h3>
-                        <p>This is a sample description. It is just a placeholder but will be used to show users what the actual workout entails.</p>
-                    </div>
-                </div>
-            </div>
+            {homeDisplay()}
         </div>
     )
 
