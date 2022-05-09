d71c26d1be45f4ea49e9d7f5f82a441a8a685c95
Matt Draghi <mtd5264@gmail.com>
2022-05-09 14:34:23 -0400

    Added sliders for other muscle groups

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -5,7 +5,15 @@ import WorkoutCard from "./WorkoutCard"
 function Workouts({user, selectedWorkout, setSelectedWorkout, setWorkoutsList, workoutsList}){
 
     const [showGenerateWorkoutModal, setShowGenerateWorkoutModal] = useState(false)
+    const [chestCount, setChestCount] = useState("0")
+    const [shouldersCount, setShouldersCount] = useState("0")
+    const [coreCount, setCoreCount] = useState("0")
+    const [bicepsCount, setBicepsCount] = useState("0")
+    const [tricepsCount, setTricepsCount] = useState("0")
     const [backCount, setBackCount] = useState("0")
+    const [legsCount, setLegsCount] = useState("0")
+    const [cardioCount, setCardioCount] = useState("0")
+
 
     useEffect(()=>{
         if(user){
@@ -50,7 +58,7 @@ function Workouts({user, selectedWorkout, setSelectedWorkout, setWorkoutsList, w
         })
     }
 
-    const handleCreateRandomWorkout = () => {
+    const handleGenerateWorkout = () => {
        setShowGenerateWorkoutModal(true)
     }
 
@@ -59,19 +67,48 @@ function Workouts({user, selectedWorkout, setSelectedWorkout, setWorkoutsList, w
             <div className="list-container">
                 <div className="list-container-header">
                     <h1>Your Workouts</h1>
-                    <button onClick={handleCreateWorkout}><span>Create Workout</span></button>
-                    <button onClick={handleCreateRandomWorkout}><span>Generate Random Workout</span></button>
+                    <button onClick={handleCreateWorkout}><span>Create Blank Workout</span></button>
+                    <button onClick={handleGenerateWorkout}><span>Generate Workout</span></button>
                     <dialog open={showGenerateWorkoutModal}>
                          <div>
                              <h2>Select Your Exercises</h2>
+                             <h3>Chest Exercises</h3>
+                             <input type="text" value={chestCount} onChange={(e)=>setChestCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={chestCount} class="slider" onChange={(e)=>setChestCount(e.target.value)}/>
+
+                             <h3>Shoulder Exercises</h3>
+                             <input type="text" value={shouldersCount} onChange={(e)=>setShouldersCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={shouldersCount} class="slider" onChange={(e)=>setShouldersCount(e.target.value)}/>
+
+                             <h3>Core Exercises</h3>
+                             <input type="text" value={coreCount} onChange={(e)=>setCoreCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={coreCount} class="slider" onChange={(e)=>setCoreCount(e.target.value)}/>
+
+                             <h3>Bicep Exercises</h3>
+                             <input type="text" value={bicepsCount} onChange={(e)=>setBicepsCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={bicepsCount} class="slider" onChange={(e)=>setBicepsCount(e.target.value)}/>
+
+                             <h3>Tricep Exercises</h3>
+                             <input type="text" value={tricepsCount} onChange={(e)=>setTricepsCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={tricepsCount} class="slider" onChange={(e)=>setTricepsCount(e.target.value)}/>
+
                              <h3>Back Exercises</h3>
                              <input type="text" value={backCount} onChange={(e)=>setBackCount(e.target.value)}/>
                              <input type="range" min="0" max="10" value={backCount} class="slider" onChange={(e)=>setBackCount(e.target.value)}/>
+
+                             <h3>Leg Exercises</h3>
+                             <input type="text" value={legsCount} onChange={(e)=>setLegsCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={legsCount} class="slider" onChange={(e)=>setLegsCount(e.target.value)}/>
+
+                             <h3>Cardio Exercises</h3>
+                             <input type="text" value={cardioCount} onChange={(e)=>setCardioCount(e.target.value)}/>
+                             <input type="range" min="0" max="10" value={cardioCount} class="slider" onChange={(e)=>setCardioCount(e.target.value)}/>
+
                          </div>
                     </dialog>
                 </div>
                 <br/>
-                <div className="card-list-container">
+                <div className="card-list-container" onClick={()=>setShowGenerateWorkoutModal(false)}>
                     {showWorkouts()}
                 </div>
             </div>
